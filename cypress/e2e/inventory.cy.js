07bd1fcbf1bd7d8351c3ac03c4260010c7d7130b
novikova-y <ysn.novikova@gmail.com>
2025-08-17 21:47:32 +0200

    Add glitch user inventory tests and fix typos

--- cypress/e2e/inventory.cy.js
+++ cypress/e2e/inventory.cy.js
@@ -8,7 +8,7 @@ describe('Inventory Page Tests', () => {
 
   describe('Standard user Scenarios', () => {
 
-  it('should display list of inventory items with name, price, and Add to cart button for standart_user', () => {
+  it('should display list of inventory items with name, price, and Add to cart button for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -21,7 +21,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should navigate to item detail page when clicking an item for standart_user', () => {
+  it('should navigate to item detail page when clicking an item for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -31,7 +31,7 @@ describe('Inventory Page Tests', () => {
     cy.get('.inventory_details_name').should('be.visible');
   });
 
-  it('should add first item to the cart and update cart badge for standart_user', () => {
+  it('should add first item to the cart and update cart badge for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -40,7 +40,7 @@ describe('Inventory Page Tests', () => {
     cy.get('.shopping_cart_badge').should('contain.text', '1');
   });
 
-  it('should sort items by price low to high for standart_user', () => {
+  it('should sort items by price low to high for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -52,7 +52,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should sort items by price high to low for standart_user', () => {
+  it('should sort items by price high to low for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -64,7 +64,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should sort items from A to Z for standart_user', () => {
+  it('should sort items from A to Z for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -76,7 +76,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should sort items from Z to A for standart_user', () => {
+  it('should sort items from Z to A for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -88,7 +88,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should show error when accessing inventory after logout for standart_user', () => {
+  it('should show error when accessing inventory after logout for standart user', () => {
     cy.login('standard');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -109,7 +109,7 @@ describe('Inventory Page Tests', () => {
 
   });
 
-  it('should not navigate to item detail page when clicking an item for problem_user', () => {
+  it('should not navigate to item detail page when clicking an item for locked user', () => {
     cy.login('locked');
     cy.get('[data-test="error"]').should('contain.text', 'Epic sadface: Sorry, this user has been locked out.');
   });
@@ -117,7 +117,7 @@ describe('Inventory Page Tests', () => {
 
   describe('Problem user Scenarios', () => {
 
-  it('should display list of inventory items with name, price, and Add to cart button for problem_user', () => {
+  it('should display list of inventory items with name, price, and Add to cart button for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -130,7 +130,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should navigate to item detail page when clicking an item for problem_user', () => {
+  it('should navigate to item detail page when clicking an item for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -140,7 +140,7 @@ describe('Inventory Page Tests', () => {
     cy.get('.inventory_details_name').should('be.visible');
   });
 
-  it('should add first item to the cart and update cart badge for problem_user', () => {
+  it('should add first item to the cart and update cart badge for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -149,7 +149,7 @@ describe('Inventory Page Tests', () => {
     cy.get('.shopping_cart_badge').should('contain.text', '1');
   });
 
-  it.skip('should sort items by price low to high for problem_user', () => {
+  it.skip('should sort items by price low to high for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -161,7 +161,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it.skip('should sort items by price high to low for problem_user', () => {
+  it.skip('should sort items by price high to low for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -173,7 +173,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should sort items from A to Z for problem_user', () => {
+  it('should sort items from A to Z for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -185,7 +185,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it.skip('should sort items from Z to A for problem_user', () => {
+  it.skip('should sort items from Z to A for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -197,7 +197,7 @@ describe('Inventory Page Tests', () => {
     });
   });
 
-  it('should show error when accessing inventory after logout for problem_user', () => {
+  it('should show error when accessing inventory after logout for problem user', () => {
     cy.login('problem');
     cy.url().should('include', '/inventory.html');
     inventoryPage.inventoryContainer.should('be.visible');
@@ -210,7 +210,99 @@ describe('Inventory Page Tests', () => {
   });
   });
 
+  describe('Glitch user Scenarios', () => {
 
+  it('should display list of inventory items with name, price, and Add to cart button for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
 
+    inventoryPage.itemCards.each(($el) => {
+      cy.wrap($el).find('.inventory_item_name').should('be.visible');
+      cy.wrap($el).find('.inventory_item_price').should('be.visible');
+      cy.wrap($el).find('button').should('contain.text', 'Add to cart');
+    });
+  });
+
+  it('should navigate to item detail page when clicking an item for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.openFirstItem();
+    cy.url().should('include', '/inventory-item');
+    cy.get('.inventory_details_name').should('be.visible');
+  });
+
+  it('should add first item to the cart and update cart badge for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.addFirstItemToCart();
+    cy.get('.shopping_cart_badge').should('contain.text', '1');
+  });
+
+  it('should sort items by price low to high for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Price (low to high)');
+    inventoryPage.getItemPrices().then((prices) => {
+      const sorted = [...prices].sort((a, b) => a - b);
+      expect(prices).to.deep.equal(sorted);
+    });
+  });
+
+  it('should sort items by price high to low for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Price (high to low)');
+    inventoryPage.getItemPrices().then((prices) => {
+      const sorted = [...prices].sort((a, b) => b - a);
+      expect(prices).to.deep.equal(sorted);
+    });
+  });
+
+  it('should sort items from A to Z for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Name (A to Z)');
+    inventoryPage.getItemNames().then((names) => {
+      const sorted = [...names].sort((a, b) => a.localeCompare(b));
+      expect(names).to.deep.equal(sorted);
+    });
+  });
+
+  it('should sort items from Z to A for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Name (Z to A)');
+    inventoryPage.getItemNames().then((names) => {
+      const sorted = [...names].sort((a, b) => b.localeCompare(a));
+      expect(names).to.deep.equal(sorted);
+    });
+  });
+
+  it('should show error when accessing inventory after logout for glitch user', () => {
+    cy.login('glitch');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.burgerMenu().should('be.visible').click();
+    inventoryPage.logoutButton().should('be.visible').click();
+    cy.visit('/inventory.html', { failOnStatusCode: false });
+    cy.get('[data-test="error"]').should('contain.text', "Epic sadface: You can only access '/inventory.html' when you are logged in.")
+      .should('be.visible');
+  });
+  });
 
 });
