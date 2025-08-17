3d29063df9d8b4c1f33f91086220c5b589cb0d12
novikova-y <ysn.novikova@gmail.com>
2025-08-17 21:59:02 +0200

    Add error user inventory tests

--- cypress/e2e/inventory.cy.js
+++ cypress/e2e/inventory.cy.js
@@ -305,4 +305,99 @@ describe('Inventory Page Tests', () => {
   });
   });
 
+  describe('Error user Scenarios', () => {
+
+  it('should display list of inventory items with name, price, and Add to cart button for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+
+    inventoryPage.itemCards.each(($el) => {
+      cy.wrap($el).find('.inventory_item_name').should('be.visible');
+      cy.wrap($el).find('.inventory_item_price').should('be.visible');
+      cy.wrap($el).find('button').should('contain.text', 'Add to cart');
+    });
+  });
+
+  it('should navigate to item detail page when clicking an item for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.openFirstItem();
+    cy.url().should('include', '/inventory-item');
+    cy.get('.inventory_details_name').should('be.visible');
+  });
+
+  it('should add first item to the cart and update cart badge for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.addFirstItemToCart();
+    cy.get('.shopping_cart_badge').should('contain.text', '1');
+  });
+
+  it.skip('should sort items by price low to high for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Price (low to high)');
+    inventoryPage.getItemPrices().then((prices) => {
+      const sorted = [...prices].sort((a, b) => a - b);
+      expect(prices).to.deep.equal(sorted);
+    });
+  });
+
+  it.skip('should sort items by price high to low for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Price (high to low)');
+    inventoryPage.getItemPrices().then((prices) => {
+      const sorted = [...prices].sort((a, b) => b - a);
+      expect(prices).to.deep.equal(sorted);
+    });
+  });
+
+  it('should sort items from A to Z for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Name (A to Z)');
+    inventoryPage.getItemNames().then((names) => {
+      const sorted = [...names].sort((a, b) => a.localeCompare(b));
+      expect(names).to.deep.equal(sorted);
+    });
+  });
+
+  it.skip('should sort items from Z to A for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.sortBy('Name (Z to A)');
+    inventoryPage.getItemNames().then((names) => {
+      const sorted = [...names].sort((a, b) => b.localeCompare(a));
+      expect(names).to.deep.equal(sorted);
+    });
+  });
+
+  it('should show error when accessing inventory after logout for error user', () => {
+    cy.login('error');
+    cy.url().should('include', '/inventory.html');
+    inventoryPage.inventoryContainer.should('be.visible');
+    inventoryPage.itemCards.should('have.length.greaterThan', 0);
+    inventoryPage.burgerMenu().should('be.visible').click();
+    inventoryPage.logoutButton().should('be.visible').click();
+    cy.visit('/inventory.html', { failOnStatusCode: false });
+    cy.get('[data-test="error"]').should('contain.text', "Epic sadface: You can only access '/inventory.html' when you are logged in.")
+      .should('be.visible');
+  });
+  });
+
 });
